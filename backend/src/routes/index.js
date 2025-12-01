7a6402c5f19fd715d737bd802171857603972dcb
Khanh Vinh <ongkhanhv2005@gmail.com>
2025-12-01 21:52:22 +0700

    feat: Finish create product

--- backend/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import authRouter from './authRoute.js';
-import adminRouter from './adminRoute.js';
-
-export default function route(app) {
-    app.use('/api/auth', authRouter);
-    app.use('/api/admins', adminRouter);
-}
