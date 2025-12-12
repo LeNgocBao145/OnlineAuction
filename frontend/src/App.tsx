aaa9ea2c3289beb09cb518578234f30bcf2ec6c3
Chorym <pham25052005@gmail.com>
2025-12-12 11:06:28 +0700

    added skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,14 @@
 import './App.css'
 
 import { BrowserRouter, Route, Routes } from 'react-router'
-import LandingPage from "./pages/index/landing"
-import SignInPage from './pages/signIn/signIn'
-import SignUpPage from './pages/signUp/signUp'
-import ErrorPage from './pages/error/error'
-import ProfilePage from './pages/profile/profile'
-import SearchPage from './pages/search/search'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<ErrorPage/>}/>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/signin' element={<SignInPage/>}/>
-        <Route path='/signup' element={<SignUpPage/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
-        <Route path='/search' element={<SearchPage/>}/>
+        {/* Common */}
+
+        {/* Catch all Error Page*/}
       </Routes>
     </BrowserRouter>
   )
