569fa943caf9048f3811c629332d3dbed9ae1cd8
= <pcbninh23@clc.fitus.edu.vn>
2026-01-05 10:51:52 +0700

    finish description

--- frontend/src/pages/product/description.tsx
+++ frontend/src/pages/product/description.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { formatDate } from "@/utils/dateUtils";
 import styles from "./description.module.css";
 import productService from "@/services/productService";
+import { Editor } from "@tinymce/tinymce-react";
+import { set } from "zod";
 
 export default function ProductDescription() {
   const MAX_DESCRIPTION_LENGTH = 500;
@@ -15,13 +17,16 @@ export default function ProductDescription() {
   if (!product) return null;
 
   const handleSaveDescription = async () => {
-    product.descriptions = [{ description: descriptionText , created_at: new Date().toISOString() }];
+    const updatedProduct = {
+      ...product,
+      descriptions: descriptionText
+        ? [{ description: descriptionText, created_at: new Date().toISOString() }]
+        : []
+    };
+
     const result = await productService.updateProduct(
-      product.id, 
-      {
-        ...product,
-        descriptions: descriptionText ? [{ description: descriptionText, created_at: new Date().toISOString() }] : []
-      }
+      product.id,
+      updatedProduct
     );
 
     if (!result.error) {
@@ -33,6 +38,7 @@ export default function ProductDescription() {
     setEditing(false);
   };
 
+
   return (
     <div className="flex flex-col justify-around items-start w-full border border-white/10 rounded-xl bg-(--third) p-4">
       <div className="mb-4 flex justify-between items-center w-full">
@@ -41,7 +47,14 @@ export default function ProductDescription() {
             Product Description
           </h1>
           {isSellerView && (
-            <button className="text-(--primary) underline" onClick={() => setEditing(!editing)}>
+            <button className="text-(--primary) underline" onClick={() => {
+              setDescriptionText(
+                product.descriptions?.length
+                  ? product.descriptions[0].description
+                  : ""
+              );
+              setEditing(!editing)}
+              }>
               Edit Description
             </button>
           )}
@@ -53,21 +66,40 @@ export default function ProductDescription() {
 
       {editing ? (
         <div className="w-full relative">
-          <textarea
-            className="w-full h-40 bg-(--secondary) text-white p-2 rounded-lg border border-white/10 focus:outline-none focus:ring-2 focus:ring-(--primary)"
-            defaultValue={product.descriptions?.map((d) => d.description).join("\n") || ""}
-            onChange={(e) => setDescriptionText(e.target.value)}
-            maxLength={MAX_DESCRIPTION_LENGTH}
+          <Editor
+            apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+            value={descriptionText}
+            onEditorChange={(content) => setDescriptionText(content)}
+            init={{
+              height: 600,
+              menubar: false,
+              plugins: [
+                "lists",
+                "link",
+                "paste",
+                "wordcount"
+              ],
+              toolbar:
+                "undo redo | bold italic underline | bullist numlist | link | removeformat",
+              content_style:
+                "body { font-family:Inter, sans-serif; font-size:14px; color:white; background-color:#111; }",
+              skin: "oxide-dark",
+              content_css: "dark",
+            }}
           />
-          <button className="mt-2 bg-(--primary) text-black font-bold py-2 px-4 rounded-lg hover:bg-(--primary)/30 transition-colors"
+
+          <button
+            className="mt-3 bg-(--primary) text-black font-bold py-2 px-4 rounded-lg hover:bg-(--primary)/30 transition-colors"
             onClick={handleSaveDescription}
           >
             Save Changes
           </button>
-          <p className="absolute bottom-2 right-2 text-white/70">{descriptionText.length} / {MAX_DESCRIPTION_LENGTH}</p>
+
+          <p className="absolute bottom-2 right-2 text-white/70">
+            {descriptionText.replace(/<[^>]*>/g, "").length} / {MAX_DESCRIPTION_LENGTH}
+          </p>
         </div>
-      ) :
-        product.descriptions?.length ? (
+      ) : product.descriptions?.length ? (
           <div className="w-full">
             {product.descriptions.map((d, idx) => (
               <div
