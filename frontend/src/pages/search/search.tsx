2aea7c3c4cc33655485e19be6e061b1bc74b9dcd
= <pcbninh23@clc.fitus.edu.vn>
2026-01-06 16:45:59 +0700

    search page formating

--- frontend/src/pages/search/search.tsx
+++ frontend/src/pages/search/search.tsx
@@ -3,7 +3,7 @@ import SearchBody from "./searchBody";
 
 export default function SearchPage() {
     return (
-        <div className="w-10/12 grid grid-cols-1 md:grid-cols-[minmax(150px,25%)_1fr] gap-4 m-auto items-start py-10">
+        <div className="w-10/12 grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-4 m-auto items-start py-10">
             <SearchFilter />
             <SearchBody />
         </div>
