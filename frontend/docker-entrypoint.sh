4c6542cbebbe7d59ed5ef2121699451b8ec825dd
LeNgocBao145 <147987387+LeNgocBao145@users.noreply.github.com>
2026-01-07 20:19:05 +0700

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -10,4 +10,8 @@ do
     find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
 done
 
+envsubst '$PORT' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
 exec "$@"
