4ef63769483f82d57e8084e144a9d51f2415029c
LeNgocBao145 <147987387+LeNgocBao145@users.noreply.github.com>
2026-01-07 20:00:24 +0700

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -5,7 +5,7 @@ for i in $(env | grep MY_APP_)
 do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
-    echo "Replacing $key with $value"
+    
     # Replace in JS and CSS files only
     find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
 done
