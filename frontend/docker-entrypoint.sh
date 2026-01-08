88fd09a5719e4c926960a9e3545c9e6e80720109
LeNgocBao145 <147987387+LeNgocBao145@users.noreply.github.com>
2026-01-08 07:46:57 +0700

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace MY_APP_ prefixed variables at runtime in JS and CSS files
-for i in $(env | grep VITE_)
+for i in $(env | grep MY_APP_)
 do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
